a584c8be05a9c75d1ed6614a8b0bb5a2bc3ac6da
Belufi <belufi.daniel@aluno.ifsp.edu.br>
2025-09-11 22:13:44 -0300

    Crud dia 11_09

---
+++ poo/classes/aluno.java
@@ -0,0 +1,32 @@
+package classes;
+public class aluno {
+    
+    private String prontuario;
+    private String nome;
+
+    public aluno(String nome, String prontuario) {
+        this.nome = nome;
+        this.prontuario = prontuario;
+    }
+
+    public void estudar(){
+        System.out.println("estudando...");
+    }
+
+    public String getProntuario(){
+        return prontuario;
+    }
+
+    public void setProntuario(String prontuario){
+        this.prontuario = prontuario;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+}
